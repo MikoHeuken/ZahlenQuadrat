7e55091985803004e78fec3bf5ec88be5be9b594
Gast1 <Gast1@DESKTOP-SV9HTJ8.Speedport_W_921V_1_44_000>
2018-11-19 21:42:45 +0100

    First commit.

---
+++ src/zahlenQuadrat.java
@@ -0,0 +1,42 @@
+
+public class zahlenQuadrat {
+
+	
+	static long Ergebnis;
+	
+	
+	public static void main(String[] args) {
+
+		int x = 100;
+		int y = 100;
+		
+		System.out.println(rechnen(x,y));
+		
+
+	}
+	
+	
+	public static long rechnen(int col, int row) {		
+		
+		
+	//	System.out.print(col+","+row);
+	//	System.out.println(" "+Ergebnis+" 1");
+		
+		if(col>1 && row>1) {rechnen(col-1, row);
+							rechnen(col, row-1);
+							rechnen(col-1, row-1);}
+	
+		if(col==1 || row==1) {Ergebnis = Ergebnis + 1;}
+		
+		
+		System.out.print(col+","+row);
+		System.out.println(" "+Ergebnis);
+
+		
+		
+		return Ergebnis;
+		
+		
+	}
+		
+}
